ac1d0a4a2693b5a39fe64d428d4b916fddd42e7a
huanlv620 <huanlv620@gmail.com>
2025-03-07 11:59:02 +0700

    dark light theme

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const headerBar = document.querySelector(".header__bar");
 const modalClose = document.querySelector(".modal__close");
 const modal = document.querySelector(".modal");
+const themeToggle = document.querySelector("#theme-toggle");
+const html = document.documentElement;
 
 headerBar.onclick = () => {
   modal.classList.add("show");
@@ -13,5 +15,16 @@ modalClose.onclick = () => {
 modal.onclick = (e) => {
   if (e.target === modal) {
     modal.classList.remove("show");
+  }
+};
+
+themeToggle.onclick = (e) => {
+  themeToggle.classList.toggle("dark-mode");
+  if (themeToggle.classList.contains("dark-mode")) {
+    themeToggle.innerHTML = "<span >🌞</span> Light Mode";
+    html.classList.add("dark");
+  } else {
+    themeToggle.innerHTML = "<span> 🌙</span> Dark Mode";
+    html.classList.remove("dark");
   }
 };
