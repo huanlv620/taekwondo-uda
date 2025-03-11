1be94a8bd30f18366affc45cff1fd3f0b56c31c2
huanlv620 <huanlv620@gmail.com>
2025-03-11 22:45:03 +0700

    fix UI

--- script.js
+++ script.js
@@ -37,3 +37,10 @@ inputCheckedToggle.addEventListener("change", (event) => {
     localStorage.removeItem("checkToggle");
   }
 });
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+const numbers = [1, 2, 3];
+const result = sum.apply(null, numbers);
+console.log(result);
